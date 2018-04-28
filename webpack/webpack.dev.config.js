d57037344e22e3330a4dca127eec32692cc270e5
github.user-at-576386355408 <brandoncookedev@gmail.com>
2018-04-28 12:38:43 -0400

    Initial commit, not working

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var parentDir = path.join(__dirname, '../');
+
+module.exports = {
+    entry: [
+        path.join(parentDir, 'webapp', 'src', 'app.js')
+    ],
+    module: {
+        loaders: [{
+            test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },{
+                test: /\.less$/,
+                loaders: ["style-loader", "css-loder", "less-loader"]
+            }
+        ]
+    },
+    output: {
+        path: parentDir + '/webapp/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: parentDir,
+        historyApiFallback: true
+    }
+}
