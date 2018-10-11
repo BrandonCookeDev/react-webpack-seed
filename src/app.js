66fee8a237ac84114c3221c5ad243a6961c5ace5
github.user-at-576386355408 <bcooke91@gmail.com>
2018-10-11 13:46:45 -0400

    Added helpful comment to app.js

--- src/app.js
+++ src/app.js
@@ -5,5 +5,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Timer from './components/Timer';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+// IMPORTANT! Remove these default references if you remove them from your Index
+// Otherwise React will not pickup the DOM elements 'app' and 'timer'
+ReactDOM.render(<App />, document.getElementById('app')) 
 ReactDOM.render(<Timer />, document.getElementById('timer'))
